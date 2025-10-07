6c92b717265e328af30cc7c8949dce2d3a76a3bb
mzckawa <mzc@cin.ufpe.br>
2025-10-07 20:35:50 -0300

    Finished question 4!

--- worksheet1/question4_triangle.c
+++ worksheet1/question4_triangle.c
@@ -36,83 +36,83 @@ int main(){
         break;
 
         case 'G':
-        printf("%s", "......A......\n.....ABA.....\n....ABCBA....\n...ABCDCBA...\n..ABCDEDCBA..\n.ABCDEFEDCAB.\nABCDEFGFEDCBA");
+        printf("%s", "......A......\n.....ABA.....\n....ABCBA....\n...ABCDCBA...\n..ABCDEDCBA..\n.ABCDEFEDCBA.\nABCDEFGFEDCBA");
         break;
 
         case 'H':
-        printf("%s", ".......A.......\n......ABA......\n.....ABCBA.....\n....ABCDCBA....\n...ABCDEDCBA...\n..ABCDEFEDCAB..\n.ABCDEFGFEDCBA.\nABCDEFGHGFEDCBA");
+        printf("%s", ".......A.......\n......ABA......\n.....ABCBA.....\n....ABCDCBA....\n...ABCDEDCBA...\n..ABCDEFEDCBA..\n.ABCDEFGFEDCBA.\nABCDEFGHGFEDCBA");
         break;
 
         case 'I':
-        printf("%s", "........A........\n.......ABA.......\n......ABCBA......\n.....ABCDCBA.....\n....ABCDEDCBA....\n...ABCDEFEDCAB...\n..ABCDEFGFEDCBA..\n.ABCDEFGHGFEDCBA.\nABCDEFGHIHGFEDCBA");
+        printf("%s", "........A........\n.......ABA.......\n......ABCBA......\n.....ABCDCBA.....\n....ABCDEDCBA....\n...ABCDEFEDCBA...\n..ABCDEFGFEDCBA..\n.ABCDEFGHGFEDCBA.\nABCDEFGHIHGFEDCBA");
         break;
 
         case 'J':
-        printf("%s", ".........A.........\n........ABA........\n.......ABCBA.......\n......ABCDCBA......\n.....ABCDEDCBA.....\n....ABCDEFEDCAB....\n...ABCDEFGFEDCBA...\n..ABCDEFGHGFEDCBA..\n.ABCDEFGHIHGFEDCBA.\nABCDEFGHIJIHGFEDCBA");
+        printf("%s", ".........A.........\n........ABA........\n.......ABCBA.......\n......ABCDCBA......\n.....ABCDEDCBA.....\n....ABCDEFEDCBA....\n...ABCDEFGFEDCBA...\n..ABCDEFGHGFEDCBA..\n.ABCDEFGHIHGFEDCBA.\nABCDEFGHIJIHGFEDCBA");
         break;
 
         case 'K':
-        printf("%s", "..........A..........\n.........ABA.........\n........ABCBA........\n.......ABCDCBA.......\n......ABCDEDCBA......\n.....ABCDEFEDCAB.....\n....ABCDEFGFEDCBA....\n...ABCDEFGHGFEDCBA...\n..ABCDEFGHIHGFEDCBA..\n.ABCDEFGHIJIHGFEDCBA.\nABCDEFGHIJKJIHGFEDCBA");
+        printf("%s", "..........A..........\n.........ABA.........\n........ABCBA........\n.......ABCDCBA.......\n......ABCDEDCBA......\n.....ABCDEFEDCBA.....\n....ABCDEFGFEDCBA....\n...ABCDEFGHGFEDCBA...\n..ABCDEFGHIHGFEDCBA..\n.ABCDEFGHIJIHGFEDCBA.\nABCDEFGHIJKJIHGFEDCBA");
         break;
 
         case 'L':
-        printf("%s", "...........A...........\n..........ABA..........\n.........ABCBA.........\n........ABCDCBA........\n.......ABCDEDCBA.......\n......ABCDEFEDCAB......\n.....ABCDEFGFEDCBA.....\n....ABCDEFGHGFEDCBA....\n...ABCDEFGHIHGFEDCBA...\n..ABCDEFGHIJIHGFEDCBA..\n.ABCDEFGHIJKJIHGFEDCBA.\nABCDEFGHIJKLKJIHGFEDCBA");
+        printf("%s", "...........A...........\n..........ABA..........\n.........ABCBA.........\n........ABCDCBA........\n.......ABCDEDCBA.......\n......ABCDEFEDCBA......\n.....ABCDEFGFEDCBA.....\n....ABCDEFGHGFEDCBA....\n...ABCDEFGHIHGFEDCBA...\n..ABCDEFGHIJIHGFEDCBA..\n.ABCDEFGHIJKJIHGFEDCBA.\nABCDEFGHIJKLKJIHGFEDCBA");
         break;
 
         case 'M':
-        printf("%s", "............A............\n...........ABA...........\n..........ABCBA..........\n.........ABCDCBA.........\n........ABCDEDCBA........\n.......ABCDEFEDCAB.......\n......ABCDEFGFEDCBA......\n.....ABCDEFGHGFEDCBA.....\n....ABCDEFGHIHGFEDCBA....\n...ABCDEFGHIJIHGFEDCBA...\n..ABCDEFGHIJKJIHGFEDCBA..\n.ABCDEFGHIJKLKJIHGFEDCBA.\nABCDEFGHIJKLMLKJIHGFECBA");
+        printf("%s", "............A............\n...........ABA...........\n..........ABCBA..........\n.........ABCDCBA.........\n........ABCDEDCBA........\n.......ABCDEFEDCBA.......\n......ABCDEFGFEDCBA......\n.....ABCDEFGHGFEDCBA.....\n....ABCDEFGHIHGFEDCBA....\n...ABCDEFGHIJIHGFEDCBA...\n..ABCDEFGHIJKJIHGFEDCBA..\n.ABCDEFGHIJKLKJIHGFEDCBA.\nABCDEFGHIJKLMLKJIHGFECBA");
         break;
 
         case 'N':
-        printf("%c", "A");
+        printf("%s", ".............A.............\n............ABA............\n...........ABCBA...........\n..........ABCDCBA..........\n.........ABCDEDCBA.........\n........ABCDEFEDCBA........\n.......ABCDEFGFEDCBA.......\n......ABCDEFGHGFEDCBA......\n.....ABCDEFGHIHGFEDCBA.....\n....ABCDEFGHIJIHGFEDCBA....\n...ABCDEFGHIJKJIHGFEDCBA...\n..ABCDEFGHIJKLKJIHGFEDCBA..\n.ABCDEFGHIJKLMLKJIHGFEDCBA.\nABCDEFGHIJKLMNMLKJIHGFEDCBA");
         break;
 
         case 'O':
-        printf("%c", "A");
+        printf("%s", "..............A..............\n.............ABA.............\n............ABCBA............\n...........ABCDCBA...........\n..........ABCDEDCBA..........\n.........ABCDEFEDCBA.........\n........ABCDEFGFEDCBA........\n.......ABCDEFGHGFEDCBA.......\n......ABCDEFGHIHGFEDCBA......\n.....ABCDEFGHIJIHGFEDCBA.....\n....ABCDEFGHIJKJIHGFEDCBA....\n...ABCDEFGHIJKLKJIHGFEDCBA...\n..ABCDEFGHIJKLMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNMLKJIHGFEDCBA.\nABCDEFGHIJKLMNONMLKJIHGFEDCBA");
         break;
 
         case 'P':
-        printf("%c", "A");
+        printf("%s", "...............A...............\n..............ABA..............\n.............ABCBA.............\n............ABCDCBA............\n...........ABCDEDCBA...........\n..........ABCDEFEDCBA..........\n.........ABCDEFGFEDCBA.........\n........ABCDEFGHGFEDCBA........\n.......ABCDEFGHIHGFEDCBA.......\n......ABCDEFGHIJIHGFEDCBA......\n.....ABCDEFGHIJKJIHGFEDCBA.....\n....ABCDEFGHIJKLKJIHGFEDCBA....\n...ABCDEFGHIJKLMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPONMLKJIHGFEDCBA");
         break;
 
         case 'Q':
-        printf("%c", "A");
+        printf("%s", "................A................\n...............ABA...............\n..............ABCBA..............\n.............ABCDCBA.............\n............ABCDEDCBA............\n...........ABCDEFEDCBA...........\n..........ABCDEFGFEDCBA..........\n.........ABCDEFGHGFEDCBA.........\n........ABCDEFGHIHGFEDCBA........\n.......ABCDEFGHIJIHGFEDCBA.......\n......ABCDEFGHIJKJIHGFEDCBA......\n.....ABCDEFGHIJKLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA");
         break;
 
         case 'R':
-        printf("%c", "A");
+        printf("%s", ".................A.................\n................ABA................\n...............ABCBA...............\n..............ABCDCBA..............\n.............ABCDEDCBA.............\n............ABCDEFEDCBA............\n...........ABCDEFGFEDCBA...........\n..........ABCDEFGHGFEDCBA..........\n.........ABCDEFGHIHGFEDCBA.........\n........ABCDEFGHIJIHGFEDCBA........\n.......ABCDEFGHIJKJIHGFEDCBA.......\n......ABCDEFGHIJKLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA");
         break;
 
         case 'S':
-        printf("%c", "A");
+        printf("%s", "..................A..................\n.................ABA.................\n................ABCBA................\n...............ABCDCBA...............\n..............ABCDEDCBA..............\n.............ABCDEFEDCBA.............\n............ABCDEFGFEDCBA............\n...........ABCDEFGHGFEDCBA...........\n..........ABCDEFGHIHGFEDCBA..........\n.........ABCDEFGHIJIHGFEDCBA.........\n........ABCDEFGHIJKJIHGFEDCBA........\n.......ABCDEFGHIJKLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA");
         break;
 
         case 'T':
-        printf("%c", "A");
+        printf("%s", "...................A...................\n..................ABA..................\n.................ABCBA.................\n................ABCDCBA................\n...............ABCDEDCBA...............\n..............ABCDEFEDCBA..............\n.............ABCDEFGFEDCBA.............\n............ABCDEFGHGFEDCBA............\n...........ABCDEFGHIHGFEDCBA...........\n..........ABCDEFGHIJIHGFEDCBA..........\n.........ABCDEFGHIJKJIHGFEDCBA.........\n........ABCDEFGHIJKLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA");
         break;
 
         case 'U':
-        printf("%c", "A");
+        printf("%s", "....................A....................\n...................ABA...................\n..................ABCBA..................\n.................ABCDCBA.................\n................ABCDEDCBA................\n...............ABCDEFEDCBA...............\n..............ABCDEFGFEDCBA..............\n.............ABCDEFGHGFEDCBA.............\n............ABCDEFGHIHGFEDCBA............\n...........ABCDEFGHIJIHGFEDCBA...........\n..........ABCDEFGHIJKJIHGFEDCBA..........\n.........ABCDEFGHIJKLKJIHGFEDCBA.........\n........ABCDEFGHIJKLMLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMNMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNONMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA");
         break;
 
         case 'V':
-        printf("%c", "A");
+        printf("%s", ".....................A.....................\n....................ABA....................\n...................ABCBA...................\n..................ABCDCBA..................\n.................ABCDEDCBA.................\n................ABCDEFEDCBA................\n...............ABCDEFGFEDCBA...............\n..............ABCDEFGHGFEDCBA..............\n.............ABCDEFGHIHGFEDCBA.............\n............ABCDEFGHIJIHGFEDCBA............\n...........ABCDEFGHIJKJIHGFEDCBA...........\n..........ABCDEFGHIJKLKJIHGFEDCBA..........\n.........ABCDEFGHIJKLMLKJIHGFEDCBA.........\n........ABCDEFGHIJKLMNMLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMNONMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDBCA");
         break;
 
         case 'W':
-        printf("%c", "A");
+        printf("%s", "......................A......................\n.....................ABA.....................\n....................ABCBA....................\n...................ABCDCBA...................\n..................ABCDEDCBA..................\n.................ABCDEFEDCBA.................\n................ABCDEFGFEDCBA................\n...............ABCDEFGHGFEDCBA...............\n..............ABCDEFGHIHGFEDCBA..............\n.............ABCDEFGHIJIHGFEDCBA.............\n............ABCDEFGHIJKJIHGFEDCBA............\n...........ABCDEFGHIJKLKJIHGFEDCBA...........\n..........ABCDEFGHIJKLMLKJIHGFEDCBA..........\n.........ABCDEFGHIJKLMNMLKJIHGFEDCBA.........\n........ABCDEFGHIJKLMNONMLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDBCA.\nABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIHGFEDCBA");
         break;
 
         case 'X':
-        printf("%c", "A");
+        printf("%s", ".......................A.......................\n......................ABA......................\n.....................ABCBA.....................\n....................ABCDCBA....................\n...................ABCDEDCBA...................\n..................ABCDEFEDCBA..................\n.................ABCDEFGFEDCBA.................\n................ABCDEFGHGFEDCBA................\n...............ABCDEFGHIHGFEDCBA...............\n..............ABCDEFGHIJIHGFEDCBA..............\n.............ABCDEFGHIJKJIHGFEDCBA.............\n............ABCDEFGHIJKLKJIHGFEDCBA............\n...........ABCDEFGHIJKLMLKJIHGFEDCBA...........\n..........ABCDEFGHIJKLMNMLKJIHGFEDCBA..........\n.........ABCDEFGHIJKLMNONMLKJIHGFEDCBA.........\n........ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA...\n..ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDBCA..\n.ABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIGHFEDCBA.\nABCDEFGHIJKLMNOPQRSTUVWXWVUTSRQPONMLKJIHGFEDCBA");
         break;
 
         case 'Y':
-        printf("%c", "A");
+        printf("%s", "........................A........................\n.......................ABA.......................\n......................ABCBA......................\n.....................ABCDCBA.....................\n....................ABCDEDCBA....................\n...................ABCDEFEDCBA...................\n..................ABCDEFGFEDCBA..................\n.................ABCDEFGHGFEDCBA.................\n................ABCDEFGHIHGFEDCBA................\n...............ABCDEFGHIJIHGFEDCBA...............\n..............ABCDEFGHIJKJIHGFEDCBA..............\n.............ABCDEFGHIJKLKJIHGFEDCBA.............\n............ABCDEFGHIJKLMLKJIHGFEDCBA............\n...........ABCDEFGHIJKLMNMLKJIHGFEDCBA...........\n..........ABCDEFGHIJKLMNONMLKJIHGFEDCBA..........\n.........ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA.........\n........ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA....\n...ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDBCA...\n..ABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIGHFEDCBA..\n.ABCDEFGHIJKLMNOPQRSTUVWXWVUTSRQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRSTUVWXYXWVUTSRQPONMLKJIHGFEDCBA");
         break;
 
         case 'Z':
-        printf("%c", "A");
+        printf("%s", ".........................A.........................\n........................ABA........................\n.......................ABCBA.......................\n......................ABCDCBA......................\n.....................ABCDEDCBA.....................\n....................ABCDEFEDCBA....................\n...................ABCDEFGFEDCBA...................\n..................ABCDEFGHGFEDCBA..................\n.................ABCDEFGHIHGFEDCBA.................\n................ABCDEFGHIJIHGFEDCBA................\n...............ABCDEFGHIJKJIHGFEDCBA...............\n..............ABCDEFGHIJKLKJIHGFEDCBA..............\n.............ABCDEFGHIJKLMLKJIHGFEDCBA.............\n............ABCDEFGHIJKLMNMLKJIHGFEDCBA............\n...........ABCDEFGHIJKLMNONMLKJIHGFEDCBA...........\n..........ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA..........\n.........ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA.........\n........ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA........\n.......ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA.......\n......ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA......\n.....ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA.....\n....ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDBCA....\n...ABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIGHFEDCBA...\n..ABCDEFGHIJKLMNOPQRSTUVWXWVUTSRQPONMLKJIHGFEDCBA..\n.ABCDEFGHIJKLMNOPQRSTUVWXYXWVUTSRQPONMLKJIHGFEDCBA.\nABCDEFGHIJKLMNOPQRSTUVWXYZYXWVUTSRQPONMLKJIHGFEDCBA");
         break;
 
     }
