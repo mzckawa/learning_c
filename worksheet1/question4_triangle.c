cba476362d996a885dab63ff4f550ed66f562b55
mzckawa <mzc@cin.ufpe.br>
2025-10-07 16:59:09 -0300

    Paused the work on question 3 to go to question 4.

---
+++ worksheet1/question4_triangle.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+
+    char letra;
+
+    scan("%c",&letra);
+
+    switch(letra){
+
+        case 'A':
+        printf("%c", 'A');
+        break;
+
+        case 'B':
+        printf("%c", ".A.\nABA");
+        break;
+
+        case 'C':
+        printf("%c", "..A..\n.ABA.\nABCBA");
+        break;
+
+        case 'D':
+        printf("%c", "...A...\n..ABA..\n.ABCBA.\nABCDCBA");
+        break;
+
+        case 'E':
+        printf("%c", "....A....\n...ABA...\n..ABCBA..\n.ABCDCBA.\nABCDEDCBA");
+        break;
+
+        case 'F':
+        printf("%c", "A");
+        break;
+
+        case 'G':
+        printf("%c", "A");
+        break;
+
+        case 'H':
+        printf("%c", "A");
+        break;
+
+        case 'I':
+        printf("%c", "A");
+        break;
+
+        case 'J':
+        printf("%c", "A");
+        break;
+
+        case 'K':
+        printf("%c", "A");
+        break;
+
+        case 'L':
+        printf("%c", "A");
+        break;
+
+        case 'M':
+        printf("%c", "A");
+        break;
+
+        case 'N':
+        printf("%c", "A");
+        break;
+
+        case 'O':
+        printf("%c", "A");
+        break;
+
+        case 'P':
+        printf("%c", "A");
+        break;
+
+        case 'Q':
+        printf("%c", "A");
+        break;
+
+        case 'R':
+        printf("%c", "A");
+        break;
+
+        case 'S':
+        printf("%c", "A");
+        break;
+
+        case 'T':
+        printf("%c", "A");
+        break;
+
+        case 'U':
+        printf("%c", "A");
+        break;
+
+        case 'V':
+        printf("%c", "A");
+        break;
+
+        case 'W':
+        printf("%c", "A");
+        break;
+
+        case 'X':
+        printf("%c", "A");
+        break;
+
+        case 'Y':
+        printf("%c", "A");
+        break;
+
+        case 'Z':
+        printf("%c", "A");
+        break;
+
+
+
+
+
+
+    }
+    return 0;
+}
+
+
+
+ABCDEDCBA
